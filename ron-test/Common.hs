11c476ee6f301926451d5b18c42e73142799c327
Yuriy Syrovetskiy <cblp@cblp.su>
2018-08-13 20:06:14 +0300

    Common test: parse frames

--- ron-test/Common.hs
+++ ron-test/Common.hs
@@ -1,13 +1,17 @@
-{-# OPTIONS -Wno-missing-signatures #-}
+{-# LANGUAGE LambdaCase #-}
 
 import qualified Data.ByteString.Lazy as BSL
 import           Data.List.Extra (dropEnd, isSuffixOf)
 import           Data.Traversable (for)
-import           Hedgehog (property, (===))
+import           GHC.Stack (HasCallStack, withFrozenCallStack)
+import           Hedgehog (MonadTest, property, (===))
+import           Hedgehog.Internal.Property (failWith)
 import           System.Directory (listDirectory)
 import           System.FilePath ((</>))
 import           Test.Tasty (TestTree, defaultMain, testGroup)
 import           Test.Tasty.Hedgehog (testProperty)
+
+import qualified RON.Text as RT
 
 main :: IO ()
 main = do
@@ -22,6 +26,14 @@ loadCases = do
             fileOut = name <> ".out.ron"
         bytesIn  <- BSL.readFile $ commonTestDir </> fileIn
         bytesOut <- BSL.readFile $ commonTestDir </> fileOut
-        pure $ testProperty name $ property $ bytesIn === bytesOut
+        pure $ testProperty name $ property $ do
+            frameIn  <- evalEitherS $ RT.parseFrame bytesIn
+            frameOut <- evalEitherS $ RT.parseFrame bytesOut
+            frameIn === frameOut
   where
     commonTestDir = "../gritzko~ron-test"
+
+evalEitherS :: (MonadTest m, HasCallStack) => Either String a -> m a
+evalEitherS = \case
+    Left  x -> withFrozenCallStack $ failWith Nothing x
+    Right a -> pure a
