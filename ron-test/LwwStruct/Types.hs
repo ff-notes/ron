899f886677a7a295004c6865fe27b89138924819
Yuriy Syrovetskiy <cblp@cblp.su>
2018-09-15 18:19:58 +0300

    Simplify schema

--- ron-test/LwwStruct/Types.hs
+++ ron-test/LwwStruct/Types.hs
@@ -10,19 +10,14 @@ import           RON.Schema
 import           RON.Schema.TH
 
 $(let
-    tExample1 = StructLww
-        { structName = "Example1"
-        , structFields =
-            [ "int1" -: Field (TAtom TAInteger) mempty
-            , "set4" -: Field (TORSet (TStructLww tExample2)) mempty
-            , "str2" -: Field (TRga char) mempty
-            , "str3" -: Field (TAtom TAString) mempty
-            ]
-        , structAnnotations = mempty{saHaskellDeriving = ["Eq", "Show"]}
-        }
-    tExample2 = StructLww
-        { structName = "Example2"
-        , structFields = ["vv5" -: Field TVersionVector mempty]
-        , structAnnotations = mempty{saHaskellDeriving = ["Eq", "Show"]}
-        }
-    in mkReplicated [DStructLww tExample1, DStructLww tExample2])
+    example1 = StructLww "Example1"
+        [ "int1" -: Field (TAtom TAInteger) mempty
+        , "set4" -: Field (TORSet (TStructLww example2)) mempty
+        , "str2" -: Field (TRga char) mempty
+        , "str3" -: Field (TAtom TAString) mempty
+        ]
+        mempty{saHaskellDeriving = ["Eq", "Show"]}
+    example2 = StructLww "Example2"
+        ["vv5" -: Field TVersionVector mempty]
+        mempty{saHaskellDeriving = ["Eq", "Show"]}
+    in mkReplicated [DStructLww example1, DStructLww example2])
