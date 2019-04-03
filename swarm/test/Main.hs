40937c85ce83cbac4dbe4e151262de1e38e561e3
Yuriy Syrovetskiy <cblp@cblp.su>
2019-04-03 18:37:13 +0300

    swarm: Remove exe

--- swarm/test/Main.hs
+++ swarm/test/Main.hs
@@ -1,2 +1,12 @@
+module Main (main) where
+
+import qualified RON.UUID as UUID
+import           Swarm.DB.Replica (get, newTextReplica)
+
 main :: IO ()
-main = pure ()
+main = do
+    replica <- newTextReplica
+    got <- get UUID.zero replica
+    case got of
+        Left errorStatus -> print errorStatus
+        Right _frame -> putStrLn "got some frame"
