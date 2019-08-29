36697b9367fd1e6b6745f337478f346b08c79f95
Yuriy Syrovetskiy <cblp@cblp.su>
2019-08-29 15:29:05 +0300

    Add ormolu

--- ron-test/Formatting.hs
+++ ron-test/Formatting.hs
@@ -1,19 +1,36 @@
+import qualified Data.ByteString.Lazy as BSL
 import Data.Monoid (Sum (Sum))
 import RON.Prelude
-import System.Directory (doesDirectoryExist, listDirectory, withCurrentDirectory)
+import RON.Util (ByteStringL)
+import System.Directory
+  ( doesDirectoryExist,
+    listDirectory,
+    withCurrentDirectory
+    )
+import System.Exit (ExitCode (ExitFailure))
 import System.FilePath ((</>), takeExtension)
+import System.Process.Typed
+  ( ExitCodeException (..),
+    byteStringInput,
+    proc,
+    readProcessStdout_,
+    setStdin
+    )
 
--- TODO ormolu | diff
 -- TODO 80
 main :: IO ()
 main =
   withCurrentDirectory projectDir $ do
     haskellFiles <- collectHaskellFiles
-    Sum n <-
-      execWriterT . for haskellFiles $ \_file -> do
-        let styleFail = True
-        when styleFail count
-    fail $ show n
+    Sum styleFails <-
+      execWriterT . for haskellFiles $ \file -> do
+        formatted <- runOrmolu file
+        diff <- runColorDiff file formatted
+        unless (BSL.null diff) count
+    let styleFailShare =
+          fromIntegral styleFails / genericLength haskellFiles :: Double
+    when (styleFailShare > 1)
+      $ fail "Bad style"
 
 isHaskellFile :: FilePath -> Bool
 isHaskellFile path = takeExtension path == ".hs"
@@ -43,3 +60,21 @@ projectDir = ".."
 
 count :: MonadWriter (Sum Int) m => m ()
 count = tell (1 :: Sum Int)
+
+runOrmolu :: MonadIO io => FilePath -> io ByteStringL
+runOrmolu file = readProcessStdout_ $ proc "ormolu" ["--", file]
+
+runColorDiff :: MonadIO io => FilePath -> ByteStringL -> io ByteStringL
+runColorDiff file content = do
+  plainDiff <-
+    liftIO $ readProcessStdout_
+      (setStdin (byteStringInput content) $ proc "diff" ["-u", "--", file, "-"])
+      `catch` \case
+        ExitCodeException {eceExitCode = ExitFailure 1} -> pure BSL.empty
+        ece -> throwIO ece
+  if BSL.null plainDiff
+    then pure BSL.empty
+    else
+      readProcessStdout_
+        $ setStdin (byteStringInput plainDiff)
+        $ proc "colordiff" []
