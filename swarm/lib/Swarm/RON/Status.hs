8a19637f83587c9f764453035500495a10025a90
Yuriy Syrovetskiy <cblp@cblp.su>
2019-10-16 17:54:51 +0300

    Style

--- swarm/lib/Swarm/RON/Status.hs
+++ swarm/lib/Swarm/RON/Status.hs
@@ -3,46 +3,50 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Swarm.RON.Status (
-    Status (..),
+module Swarm.RON.Status
+  ( Status (..),
     decode,
     decoding,
     decoding_,
     with,
+
     -- * Statuses
     ok,
     noType,
     notFound,
     notOpen,
-) where
+  )
+where
 
-import           Control.Exception (bracket)
-import           Data.ByteString (ByteString)
+import Control.Exception (bracket)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.Map.Strict as Map
-import           Data.Proxy (Proxy)
-import           Foreign (Ptr)
-import           Language.C.Inline.Context (ctxTypesTable)
-import           Language.C.Inline.Cpp (withPtrs_)
+import Data.Proxy (Proxy)
+import Foreign (Ptr)
+import Language.C.Inline.Context (ctxTypesTable)
+import Language.C.Inline.Cpp (withPtrs_)
 import qualified Language.C.Inline.Cpp as Cpp
-import           Language.C.Types (TypeSpecifier (TypeName))
-
-import           RON.UUID (UUID (UUID))
+import Language.C.Types (TypeSpecifier (TypeName))
+import RON.UUID (UUID (UUID))
 
 -- | Class @ron::Status@
-data Status = Status{code :: UUID, comment :: ByteString}
-    deriving (Eq, Show)
+data Status = Status {code :: UUID, comment :: ByteString}
+  deriving (Eq, Show)
 
-Cpp.context
-    $   Cpp.cppCtx
-    <>  mempty
-        { ctxTypesTable = Map.singleton (TypeName "Status") [t| Proxy Status |]
-        }
+Cpp.context $
+  Cpp.cppCtx
+    <> mempty
+      { ctxTypesTable = Map.singleton (TypeName "Status") [t|Proxy Status|]
+      }
+
 Cpp.include "<swarm/ron/status.hpp>"
+
 Cpp.verbatim "typedef ron::Status Status;"
 
 ok :: UUID
-ok = UUID
+ok =
+  UUID
     [Cpp.pure| uint64_t { uint64_t(Status::OK.code().value ()) } |]
     [Cpp.pure| uint64_t { uint64_t(Status::OK.code().origin()) } |]
 
@@ -50,7 +54,8 @@ ok = UUID
 --     | NOT_IMPLEMENTED
 
 notFound :: UUID
-notFound = UUID
+notFound =
+  UUID
     [Cpp.pure| uint64_t { uint64_t(Status::NOT_FOUND.code().value ()) } |]
     [Cpp.pure| uint64_t { uint64_t(Status::NOT_FOUND.code().origin()) } |]
 
@@ -65,12 +70,14 @@ notFound = UUID
 --     | BADVALUE
 
 noType :: UUID
-noType = UUID
+noType =
+  UUID
     [Cpp.pure| uint64_t { uint64_t(Status::NOTYPE.code().value ()) } |]
     [Cpp.pure| uint64_t { uint64_t(Status::NOTYPE.code().origin()) } |]
 
 notOpen :: UUID
-notOpen = UUID
+notOpen =
+  UUID
     [Cpp.pure| uint64_t { uint64_t(Status::NOTOPEN.code().value ()) } |]
     [Cpp.pure| uint64_t { uint64_t(Status::NOTOPEN.code().origin()) } |]
 
@@ -87,27 +94,29 @@ notOpen = UUID
 
 decode :: Ptr (Proxy Status) -> IO Status
 decode statusPtr = do
-    (x, y, ptr, len) <- withPtrs_ $ \(x, y, ptr, len) -> [Cpp.block| void {
-        Status & status = * $(Status * statusPtr);
-        * $(uint64_t * x)      = uint64_t(status.code().value());
-        * $(uint64_t * y)      = uint64_t(status.code().origin());
-        * $(char const ** ptr) = status.comment().data();
-        * $(size_t * len)      = status.comment().length();
+  (x, y, ptr, len) <- withPtrs_ $ \(x, y, ptr, len) ->
+    [Cpp.block| void {
+      Status & status = * $(Status * statusPtr);
+      * $(uint64_t * x)      = uint64_t(status.code().value());
+      * $(uint64_t * y)      = uint64_t(status.code().origin());
+      * $(char const ** ptr) = status.comment().data();
+      * $(size_t * len)      = status.comment().length();
     } |]
-    comment <- BS.packCStringLen (ptr, fromIntegral len)
-    pure Status{code = UUID x y, comment}
+  comment <- BS.packCStringLen (ptr, fromIntegral len)
+  pure Status {code = UUID x y, comment}
 
 with :: (Ptr (Proxy Status) -> IO a) -> IO a
-with = bracket
+with =
+  bracket
     [Cpp.exp| Status * { new Status } |]
     (\p -> [Cpp.block| void { delete $(Status * p); } |])
 
 decoding :: (Ptr (Proxy Status) -> IO a) -> IO (Status, a)
 decoding action =
-    with $ \ptr -> do
-        a <- action ptr
-        status <- decode ptr
-        pure (status, a)
+  with $ \ptr -> do
+    a <- action ptr
+    status <- decode ptr
+    pure (status, a)
 
 decoding_ :: (Ptr (Proxy Status) -> IO ()) -> IO Status
 decoding_ = fmap fst . decoding
