0312371c32da973dd93a1f4e121ab3a4a8e00d93
Yuriy Syrovetskiy <cblp@cblp.su>
2019-10-23 16:54:46 +0300

    Style

--- demo/text-editor-brick/Main.hs
+++ demo/text-editor-brick/Main.hs
@@ -3,128 +3,161 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Brick (App (App), BrickEvent (AppEvent, VtyEvent), EventM,
-                        Next, Widget, attrMap, continue, customMain, halt,
-                        showFirstCursor, txt, (<=>))
+import Brick
+  ( (<=>),
+    App (App),
+    BrickEvent (AppEvent, VtyEvent),
+    EventM,
+    Next,
+    Widget,
+    attrMap,
+    continue,
+    customMain,
+    halt,
+    showFirstCursor,
+    txt,
+  )
 import qualified Brick
-import           Brick.BChan (BChan, newBChan, writeBChan)
-import           Brick.Widgets.Border (border)
-import           Brick.Widgets.Edit (Editor, applyEdit, editorText,
-                                     getEditContents, handleEditorEvent,
-                                     renderEditor)
-import           Control.Concurrent (forkIO, threadDelay)
-import           Control.Monad (forever, void)
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Text (Text)
+import Brick.BChan (BChan, newBChan, writeBChan)
+import Brick.Widgets.Border (border)
+import Brick.Widgets.Edit
+  ( Editor,
+    applyEdit,
+    editorText,
+    getEditContents,
+    handleEditorEvent,
+    renderEditor,
+  )
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever, void)
+import Control.Monad.IO.Class (liftIO)
+import Data.Text (Text)
 import qualified Data.Text as Text
-import           Data.Text.Zipper (TextZipper, cursorPosition, moveCursor,
-                                   textZipper)
+import Data.Text.Zipper
+  ( TextZipper,
+    cursorPosition,
+    moveCursor,
+    textZipper,
+  )
 import qualified Data.Text.Zipper as TextZipper
-import           Graphics.Vty (Event (EvKey), Key (KEsc), defAttr,
-                               defaultConfig, mkVty)
-import           RON.Data (evalObjectState, execObjectState)
-import           RON.Data.RGA (RgaString)
+import Graphics.Vty
+  ( Event (EvKey),
+    Key (KEsc),
+    defAttr,
+    defaultConfig,
+    mkVty,
+  )
+import RON.Data (evalObjectState, execObjectState)
+import RON.Data.RGA (RgaString)
 import qualified RON.Data.RGA as RGA
-import           RON.Storage.Backend (DocId (DocId), Document (Document),
-                                      createVersion, objectFrame)
-import           RON.Storage.FS (loadDocument, runStorage)
+import RON.Storage.Backend
+  ( DocId (DocId),
+    Document (Document),
+    createVersion,
+    objectFrame,
+  )
+import RON.Storage.FS (loadDocument, runStorage)
 import qualified RON.Storage.FS as Storage
-
-import           Types ()
+import Types ()
 
 theDoc :: DocId RgaString
 theDoc = DocId "B3QCFGMHK1QJS-2005CRP400492"
 
 main :: IO ()
 main = do
-    let dataDir = "demo/data"
-    h <- Storage.newHandle dataDir
-    (document, text) <- runStorage h $ do
-        document@Document{objectFrame} <- loadDocument theDoc
-        text <- evalObjectState objectFrame RGA.getText
-        pure (document, text)
-    rhythm <- mkRhythm
-    runUI rhythm (mkApp h) MyState{editor = mkEditor text, document}
+  let dataDir = "demo/data"
+  h <- Storage.newHandle dataDir
+  (document, text) <- runStorage h $ do
+    document@Document {objectFrame} <- loadDocument theDoc
+    text <- evalObjectState objectFrame RGA.getText
+    pure (document, text)
+  rhythm <- mkRhythm
+  runUI rhythm (mkApp h) MyState {editor = mkEditor text, document}
 
 mkEditor :: Text -> Editor Text ()
 mkEditor = editorText () Nothing
 
 mkRhythm :: IO (BChan ())
 mkRhythm = do
-    chan <- newBChan 1
-    _ <- forkIO $ forever $ do
-        writeBChan chan ()
-        threadDelay 1000000
-    pure chan
+  chan <- newBChan 1
+  _ <- forkIO $ forever $ do
+    writeBChan chan ()
+    threadDelay 1000000
+  pure chan
 
 runUI :: BChan () -> App s () () -> s -> IO ()
 runUI chan app initialState = do
-    let buildVty = mkVty defaultConfig
-    initialVty <- buildVty
-    void $ customMain initialVty buildVty (Just chan) app initialState
+  let buildVty = mkVty defaultConfig
+  initialVty <- buildVty
+  void $ customMain initialVty buildVty (Just chan) app initialState
 
-data MyState = MyState{editor :: Editor Text (), document :: Document RgaString}
+data MyState
+  = MyState {editor :: Editor Text (), document :: Document RgaString}
 
 mkApp :: Storage.Handle -> App MyState () ()
 mkApp storage = App
-    { appAttrMap = const $ attrMap defAttr []
-    , appChooseCursor = showFirstCursor
-    , appDraw = draw
-    , appHandleEvent = handleEvent storage
-    , appStartEvent = pure
-    }
+  { appAttrMap = const $ attrMap defAttr [],
+    appChooseCursor = showFirstCursor,
+    appDraw = draw,
+    appHandleEvent = handleEvent storage,
+    appStartEvent = pure
+  }
 
 draw :: MyState -> [Widget ()]
-draw MyState{editor} =
-    [   border (renderEditor (txt . Text.unlines) True editor)
-    <=> txt "Esc -> exit"
-    ]
+draw MyState {editor} =
+  [ border (renderEditor (txt . Text.unlines) True editor)
+      <=> txt "Esc -> exit"
+  ]
 
-handleEvent
-    :: Storage.Handle -> MyState -> BrickEvent () () -> EventM () (Next MyState)
-handleEvent storage state@MyState{editor} = \case
-    VtyEvent ve -> case ve of
-        EvKey KEsc [] -> do
-            state' <- liftIO $ sync storage state False
-            halt state'
-        _ -> do
-            editor' <- handleEditorEvent ve editor
-            continue state{editor = editor'}
-    AppEvent () -> do
-        state' <- liftIO $ sync storage state True
-        continue state'
-    _ -> continue state
+handleEvent ::
+  Storage.Handle -> MyState -> BrickEvent () () -> EventM () (Next MyState)
+handleEvent storage state@MyState {editor} = \case
+  VtyEvent ve -> case ve of
+    EvKey KEsc [] -> do
+      state' <- liftIO $ sync storage state False
+      halt state'
+    _ -> do
+      editor' <- handleEditorEvent ve editor
+      continue state {editor = editor'}
+  AppEvent () -> do
+    state' <- liftIO $ sync storage state True
+    continue state'
+  _ -> continue state
 
 sync :: Storage.Handle -> MyState -> Bool -> IO MyState
-sync storage state@MyState{editor, document} reload =
-    runStorage storage $ do
-        do  let Document{objectFrame} = document
-            objectFrame' <-
-                execObjectState objectFrame $
-                RGA.editText $ Text.unlines $ getEditContents editor
-            createVersion (Just (theDoc, document)) objectFrame'
-        if reload then do
-            document'@Document{objectFrame} <- loadDocument theDoc
-            text <- evalObjectState objectFrame RGA.getText
-            let editor' = applyEdit (replaceZipper text) editor
-            pure MyState{editor = editor', document = document'}
-        else
-            pure state
+sync storage state@MyState {editor, document} reload =
+  runStorage storage $ do
+    do
+      let Document {objectFrame} = document
+      objectFrame' <-
+        execObjectState objectFrame
+          $ RGA.editText
+          $ Text.unlines
+          $ getEditContents editor
+      createVersion (Just (theDoc, document)) objectFrame'
+    if reload
+      then do
+        document'@Document {objectFrame} <- loadDocument theDoc
+        text <- evalObjectState objectFrame RGA.getText
+        let editor' = applyEdit (replaceZipper text) editor
+        pure MyState {editor = editor', document = document'}
+      else pure state
 
 -- | Replace content in zipper keeping cursor
 -- TODO save RGA ids and keep position after RGA changes
 replaceZipper :: Text -> TextZipper Text -> TextZipper Text
-replaceZipper text zipper = moveCursorClosely originalPosition zipper' where
+replaceZipper text zipper = moveCursorClosely originalPosition zipper'
+  where
     zipper' = textZipper (Text.lines text) Nothing
     originalPosition = cursorPosition zipper
 
 moveCursorClosely :: (Int, Int) -> TextZipper Text -> TextZipper Text
 moveCursorClosely (row, col) tz
-    | row < 0     = moveCursor (0       , 0   ) tz
-    | row >= rows = moveCursor (rows - 1, 0   ) tz
-    | col < 0     = moveCursor (row     , 0   ) tz
-    | col > cols  = moveCursor (row     , cols) tz
-    | otherwise   = moveCursor (row     , col ) tz
+  | row < 0 = moveCursor (0, 0) tz
+  | row >= rows = moveCursor (rows - 1, 0) tz
+  | col < 0 = moveCursor (row, 0) tz
+  | col > cols = moveCursor (row, cols) tz
+  | otherwise = moveCursor (row, col) tz
   where
     t = TextZipper.getText tz
     rows = length t
