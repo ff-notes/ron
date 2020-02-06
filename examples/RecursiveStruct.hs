dd814342e2a13aa04d7e5f019b9a042f824cf2c8
Yuriy Syrovetskiy <cblp@cblp.su>
2020-02-06 17:18:24 +0300

    Add RecursiveStruct example

---
+++ examples/RecursiveStruct.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+import RON.Data
+import RON.Data.RGA
+import RON.Schema.TH
+import RON.Storage.FS as Storage
+
+[mkReplicated|
+    (struct_set Note
+        #ron/collection "note"
+        (active lww     Bool)
+        (text   merge   RgaString)
+        (links  set     Note))
+    |]
+
+instance Collection Note where
+    collectionName = "note"
+
+main :: IO ()
+main = do
+    let dataDir = "./data/"
+    h <- Storage.newHandle dataDir
+    runStorage h $ do
+        obj <-
+            newObjectFrame
+                Note{
+                    active = Just True,
+                    text = Just $ RGA "Write a task manager"
+                    }
+        createDocument obj
