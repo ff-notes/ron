6dcd8f7a0474aef05c223f80c1ebc72da8d39a66
Yuriy Syrovetskiy <cblp@cblp.su>
2020-06-26 21:31:15 +0300

    Add ORSet.add

--- demo/chat/Main.hs
+++ demo/chat/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 
+import qualified Data.Text as Text
 import           System.Environment (getArgs, getProgName)
 
 import           RON.Data.ORSet (ORSetRep)
+import qualified RON.Data.ORSet.Experimental as ORSet
 import           RON.Store (createObject, getObjects)
 import           RON.Store.FS (newHandle, runStore)
+import           RON.Types (Atom (AString))
 import           RON.Types.Experimental (CollectionName)
 
 type Message = ORSetRep
@@ -22,8 +25,15 @@ main = do
     [] -> do
       messagesResult <- runStore db $ getObjects @Message messagesCollection
       print messagesResult
-    [_name, _text] -> do
-      messageRef <- runStore db $ createObject @Message messagesCollection
+    [username, text] -> do
+      messageRef <-
+        runStore db $ do
+          messageRef <- createObject @Message messagesCollection
+          ORSet.add_
+            messageRef
+            [AString "username", AString $ Text.pack username]
+          ORSet.add_ messageRef [AString "text", AString $ Text.pack text]
+          pure messageRef
       putStrLn $ "created message: " <> show messageRef
     _ ->
       putStrLn $
---
+++ ron-rdt/lib/RON/Data/ORSet/Experimental.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module RON.Data.ORSet.Experimental (add, add_) where
+
+import           RON.Prelude
+
+import           RON.Event (ReplicaClock, getEventUuid)
+import           RON.Store (MonadStore, addPatch)
+import           RON.Types (Op (..), Payload, UUID)
+import           RON.Types.Experimental (ObjectRef (..))
+
+-- | Add value to the set. Return the reference to the set item.
+add :: (MonadStore m, ReplicaClock m) => ObjectRef a -> Payload -> m UUID
+add (ObjectRef collection object) payload = do
+  opId <- getEventUuid
+  addPatch collection object [Op{opId, refId = object, payload}]
+  pure opId
+
+-- | Add value to the set.
+add_ :: (MonadStore m, ReplicaClock m) => ObjectRef a -> Payload -> m ()
+add_ objectRef payload = void $ add objectRef payload
