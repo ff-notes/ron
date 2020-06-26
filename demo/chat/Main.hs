aca957d21ed9349b1158e28ff5850a647726bc96
Yuriy Syrovetskiy <cblp@cblp.su>
2020-06-26 11:04:44 +0300

    demo-chat: Add help message and a stub for posting a message

--- demo/chat/Main.hs
+++ demo/chat/Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 
+import           System.Environment (getArgs, getProgName)
+
 import           RON.Store (getObjects)
 import           RON.Store.FS (newHandle, runStore)
 import           RON.Types.Experimental (CollectionName)
@@ -13,5 +15,21 @@ messagesCollection = "messages"
 main :: IO ()
 main = do
   db <- newHandle "./data"
-  messagesResult <- runStore db $ getObjects @_ @Message messagesCollection
-  print messagesResult
+  progName <- getProgName
+  args <- getArgs
+  case args of
+    [] -> do
+      messagesResult <- runStore db $ getObjects @_ @Message messagesCollection
+      print messagesResult
+    [_name, _text] -> undefined
+    _ ->
+      putStrLn $
+      unlines
+        [ "Usage:"
+        , ""
+        , unwords [progName]
+        , "\t show all messages"
+        , ""
+        , unwords [progName, "NAME TEXT"]
+        , "\t post a message"
+        ]
