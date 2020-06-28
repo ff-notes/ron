fff07255b1a89a0c5c080205b3244863fc55c292
Yuriy Syrovetskiy <cblp@cblp.su>
2020-06-28 19:42:14 +0300

    ron-tool: Add options parsing

--- ron-tool/Main.hs
+++ ron-tool/Main.hs
@@ -1,2 +1,35 @@
+import           Options.Applicative (InfoMod, Parser, ParserInfo,
+                                      ParserPrefs (..), customExecParser,
+                                      defaultPrefs, fullDesc, helper, info,
+                                      progDesc, (<**>))
+
 main :: IO ()
-main = pure ()
+main = do
+  parseOptions >>= print
+
+data Options = Options
+  deriving (Show)
+
+parseOptions :: IO Options
+parseOptions =
+  customExecParser prefs parserInfo
+  where
+    parserInfo = i parser "RON tool"
+
+    parser = do
+      pure Options{}
+
+prefs :: ParserPrefs
+prefs =
+  defaultPrefs
+    { prefDisambiguate = True
+    , prefHelpLongEquals = True
+    , prefMultiSuffix = "..."
+    , prefShowHelpOnError = True
+    }
+
+i :: Parser a -> String -> ParserInfo a
+i prsr desc = i_ prsr desc mempty
+
+i_ :: Parser a -> String -> InfoMod a -> ParserInfo a
+i_ prsr desc m = info (prsr <**> helper) $ fullDesc <> progDesc desc <> m
