a6fcaa0945d6dfb53c3939814ed05525110f2b59
Yuriy Syrovetskiy <cblp@cblp.su>
2020-07-12 18:28:50 +0300

    demo-chat: Add server stub

--- demo/chat/NetNode.hs
+++ demo/chat/NetNode.hs
@@ -1,9 +1,29 @@
 module NetNode (worker) where
 
+import           Control.Monad.Extra (whenJust)
+import qualified Network.WebSockets as WS
+import           RON.Text.Parse (parseOpenFrame)
+import           RON.Types (OpenFrame)
+
 worker ::
   -- | Server port to listen
   Maybe Int ->
   -- | Other peer ports to connect (only localhost)
   [Int] ->
   IO ()
-worker _listen _peers = pure ()
+worker listen _peers =
+  whenJust listen $ \port ->
+    WS.runServer "::" port serverApp
+
+serverApp :: WS.ServerApp
+serverApp pendingConnection = do
+  conn <- WS.acceptRequest pendingConnection
+  WS.withPingThread conn 30 (pure ()) $ do
+    frameData <- WS.receiveData conn
+    case parseOpenFrame frameData of
+      Left e -> error e
+      Right frame -> handleIncomingFrame frame
+    pure ()
+
+handleIncomingFrame :: OpenFrame -> IO ()
+handleIncomingFrame = undefined
