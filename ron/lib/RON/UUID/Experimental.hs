752852568e8860f0d6c6150d11b8bd5a78954319
Yuriy Syrovetskiy <cblp@cblp.su>
2021-04-27 15:07:07 +0300

    Add more experimental lenses

--- ron/lib/RON/UUID/Experimental.hs
+++ ron/lib/RON/UUID/Experimental.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE RankNTypes #-}
 
-module RON.UUID.Experimental (value) where
+module RON.UUID.Experimental (variety, value, variant, version, origin) where
 
 import           RON.Prelude
 
-import           Data.Bits ((.&.), (.|.))
+import           Data.Bits (shiftL, shiftR, (.&.), (.|.))
 
-import           RON.Util.Word (Word60, ls60, safeCast)
 import           RON.UUID (UUID (..))
+import           RON.Util.Word (Word2, Word4, Word60, leastSignificant2,
+                                leastSignificant4, leastSignificant60, safeCast)
 
 -- data UuidFields = UuidFields
 --     { uuidVariety :: !Word4
@@ -17,11 +18,38 @@ import           RON.UUID (UUID (..))
 --     , uuidOrigin  :: !Word60
 --     }
 
+variety :: Lens' UUID Word4
+variety =
+  lens
+    (\(UUID x _) -> leastSignificant4 $ x `shiftR` 60)
+    (\(UUID x y) v ->
+      UUID (x .&. 0x0FFFFFFFFFFFFFFF .|. (safeCast v `shiftL` 60)) y)
+
 value :: Lens' UUID Word60
 value =
   lens
-    (\(UUID x _) -> ls60 x)
+    (\(UUID x _) -> leastSignificant60 x)
     (\(UUID x y) v -> UUID (x .&. 0xF000000000000000 .|. safeCast v) y)
+
+variant :: Lens' UUID Word2
+variant =
+  lens
+    (\(UUID _ y) -> leastSignificant2 $ y `shiftR` 62)
+    (\(UUID x y) v ->
+      UUID x (y .&. 0x3FFFFFFFFFFFFFFF .|. (safeCast v `shiftL` 62)))
+
+version :: Lens' UUID Word2
+version =
+  lens
+    (\(UUID _ y) -> leastSignificant2 $ y `shiftR` 60)
+    (\(UUID x y) v ->
+      UUID x (y .&. 0xCFFFFFFFFFFFFFFF .|. (safeCast v `shiftL` 60)))
+
+origin :: Lens' UUID Word60
+origin =
+  lens
+    (\(UUID _ y) -> leastSignificant60 y)
+    (\(UUID x y) v -> UUID x (y .&. 0xF000000000000000 .|. safeCast v))
 
 type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
 
