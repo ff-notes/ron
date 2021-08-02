1959e02a43c3a16641d6bb9d4f1b6b2b2fefff1b
Yuriy Syrovetskiy <cblp@cblp.su>
2021-08-02 11:00:41 +0300

    GTree: decouple type tag and internal state type

--- ron-rdt/lib/RON/Data/GTree.hs
+++ ron-rdt/lib/RON/Data/GTree.hs
@@ -19,29 +19,26 @@ import           RON.Types (Op (..), OpenFrame, UUID)
 import           RON.Types.Experimental (Patch (..), Ref (..))
 
 -- | Grow-only tree
-newtype GTree = GTree (HashMap UUID (HashSet Op))
+data GTree
 
-readFrame :: UUID -> OpenFrame -> GTree
+type GTreeState = HashMap UUID (HashSet Op)
+
+readFrame :: UUID -> OpenFrame -> GTreeState
 readFrame object ops =
-  GTree $
-    HashMap.fromListWith
-      (<>)
-      [ (refId, HashSet.singleton op)
-      | op@Op{opId, refId} <- ops, opId /= object
-      ]
+  HashMap.fromListWith
+    (<>)
+    [(refId, HashSet.singleton op) | op@Op{opId, refId} <- ops, opId /= object]
 
-toForest :: UUID -> GTree -> Forest Op
-toForest object (GTree children) = go object where
+toForest :: UUID -> GTreeState -> Forest Op
+toForest object children = go object where
   go i =
     [ Node op $ go opId
     | op@Op{opId} <- toList $ HashMap.lookupDefault mempty i children
     ]
 
 loadForest :: (MonadE m, MonadStore m) => Ref GTree -> m (Forest Op)
-loadForest object@(Ref objectId _) = do
-  ops <- loadSubObjectLog object mempty
-  let state = readFrame objectId ops
-  pure $ toForest objectId state
+loadForest ref@(Ref oid _) =
+  toForest oid . readFrame oid <$> loadSubObjectLog ref mempty
 
 insert :: (MonadStore m, ReplicaClock m) => Ref GTree -> UUID -> m ()
 insert (Ref object prefix) parent = do
