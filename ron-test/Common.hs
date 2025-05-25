6542ab56ea8918da217d57c156a9e7c579cf19e6
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-05-25 18:45:40 +0200

    Fix head and tail usage

--- ron-test/Common.hs
+++ ron-test/Common.hs
@@ -1,36 +1,44 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 
-import           RON.Prelude
+import RON.Prelude
 
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Lazy.Char8 as BSLC
-import           Data.Either (partitionEithers)
-import           Data.Foldable (concat)
-import           Data.Generics (gcompare)
-import           Data.List.Extra (dropEnd, head)
-import qualified Data.Map.Merge.Strict as Map
-import qualified Data.Map.Strict as Map
-import           Hedgehog (Property, property, (===))
-import           System.Directory (listDirectory)
-import           System.Directory.Tree (DirTree(File, file),
-                                        AnchoredDirTree((:/)), flattenDir,
-                                        readDirectoryWith)
-import           System.FilePath ((</>))
-import           Test.Tasty (TestTree, defaultMain, testGroup)
-import           Test.Tasty.Hedgehog (testProperty)
-import           Test.Tasty.HUnit (assertEqual, assertFailure, testCase)
+import Data.ByteString.Lazy qualified as BSL
+import Data.ByteString.Lazy.Char8 qualified as BSLC
+import Data.Either (partitionEithers)
+import Data.Foldable (concat)
+import Data.Generics (gcompare)
+import Data.List.Extra (dropEnd)
+import Data.Map.Merge.Strict qualified as Map
+import Data.Map.Strict qualified as Map
+import Hedgehog (Property, property, (===))
+import System.Directory (listDirectory)
+import System.Directory.Tree (
+    AnchoredDirTree ((:/)),
+    DirTree (File, file),
+    flattenDir,
+    readDirectoryWith,
+ )
+import System.FilePath ((</>))
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, assertFailure, testCase)
+import Test.Tasty.Hedgehog (testProperty)
 
-import           RON.Data (reduceWireFrame)
-import qualified RON.Text as RT
-import qualified RON.Text.Serialize as RT
-import           RON.Types (ClosedOp (..), UUID,
-                            WireChunk (Closed, Query, Value),
-                            WireReducedChunk (..))
-import qualified RON.UUID as UUID
+import RON.Data (reduceWireFrame)
+import RON.Text qualified as RT
+import RON.Text.Serialize qualified as RT
+import RON.Types (
+    ClosedOp (..),
+    UUID,
+    WireChunk (Closed, Query, Value),
+    WireReducedChunk (..),
+ )
+import RON.UUID qualified as UUID
 
 main :: IO ()
 main = do
@@ -41,62 +49,74 @@ loadCases :: IO [TestTree]
 loadCases = do
     inOutFiles <-
         filter (".in.ron" `isSuffixOf`) <$> listDirectory commonTestDir
-    testsInOut <- for inOutFiles $ \fileIn -> do
+    testsInOut <- for inOutFiles \fileIn -> do
         let name = dropEnd 7 fileIn
             fileOut = name <> ".out.ron"
-        chunksIn0  <- readChunks $ commonTestDir </> fileIn
+        chunksIn0 <- readChunks $ commonTestDir </> fileIn
         chunksOut0 <- readChunks $ commonTestDir </> fileOut
-        pure $ testGroup name $
-            case collectLefts (chunksIn0, chunksOut0) of
-                Left errs ->
-                    [testCase part $ assertFailure err | (part, err) <- errs]
-                Right (chunksIn1, chunksOut1) ->
-                    [ testProperty (BSLC.unpack $ RT.serializeUuid obj) $
-                        reduceAndCompareChunks chunksIn2 chunksOut2
-                    | not $ "06" `isPrefixOf` name
-                    , (obj, (chunksIn2, chunksOut2)) <-
-                        Map.assocs $ zipDef [] [] chunksIn1 chunksOut1
-                    ]
+        pure $
+            testGroup name $
+                case collectLefts (chunksIn0, chunksOut0) of
+                    Left errs ->
+                        [ testCase part $ assertFailure err
+                        | (part, err) <- errs
+                        ]
+                    Right (chunksIn1, chunksOut1) ->
+                        [ testProperty (BSLC.unpack $ RT.serializeUuid obj) $
+                            reduceAndCompareChunks chunksIn2 chunksOut2
+                        | not $ "06" `isPrefixOf` name
+                        , (obj, (chunksIn2, chunksOut2)) <-
+                            Map.assocs $ zipDef [] [] chunksIn1 chunksOut1
+                        ]
     noteFiles <- collectFiles "../.ff"
     when (null noteFiles) $ fail "there must be some ff files"
     testsNote <-
         for noteFiles $ \fileIn -> do
-            chunks0  <- readChunks fileIn
-            pure $ testGroup fileIn $
-                case chunks0 of
-                    Left err -> [testCase fileIn $ assertFailure err]
-                    Right c  -> let
-                        es = Map.elems c
-                        (lefts, rights) =
-                            partitionEithers $
-                            fmap zipWithWireFrameRoundtrip es
-                        newTestCase ks =
-                            testCase
-                                ("comparing roundtrip for object " <> show ks)
-                        in
-                        map (testCase fileIn . assertFailure) lefts ++
-                        [ newTestCase ks $ assertEqual "not equal" w w'
-                        | (w, ks, w') <- rights
-                        ]
+            chunks0 <- readChunks fileIn
+            pure $
+                testGroup fileIn $
+                    case chunks0 of
+                        Left err -> [testCase fileIn $ assertFailure err]
+                        Right c ->
+                            let es = Map.elems c
+                                (lefts, rights) =
+                                    partitionEithers $
+                                        fmap zipWithWireFrameRoundtrip es
+                                newTestCase ks =
+                                    testCase
+                                        ( "comparing roundtrip for object "
+                                            <> show ks
+                                        )
+                             in map (testCase fileIn . assertFailure) lefts
+                                    ++ [ newTestCase ks $
+                                        assertEqual "not equal" w w'
+                                       | (w, ks, w') <- rights
+                                       ]
     pure $ testsInOut ++ testsNote
   where
     commonTestDir = "../gritzko~ron-test"
+
     readChunks file = do
         bytes <- BSL.readFile file
         let eFrame = RT.parseWireFrame bytes
         pure $ filterWireFrame eFrame
+
     filterWireFrame eFrame =
         groupObjects . filter isRelevant <$> eFrame
+
     wireFrameRoundtrip =
         filterWireFrame . RT.parseWireFrame . RT.serializeWireFrame
-    zipWithWireFrameRoundtrip w = let
-        r = wireFrameRoundtrip w
-        f x = (w, head $ Map.keys x, concat . Map.elems $ x)
-        in f <$> r
-    zipDef a b = Map.merge
-        (Map.mapMissing $ const (,b))
-        (Map.mapMissing $ const (a,))
-        (Map.zipWithMatched $ const (,))
+
+    zipWithWireFrameRoundtrip w =
+        let r = wireFrameRoundtrip w
+            f x = (w, listToMaybe $ Map.keys x, concat . Map.elems $ x)
+         in f <$> r
+
+    zipDef a b =
+        Map.merge
+            (Map.mapMissing $ const (,b))
+            (Map.mapMissing $ const (a,))
+            (Map.zipWithMatched $ const (,))
 
 collectFiles :: String -> IO [String]
 collectFiles d = do
@@ -108,7 +128,7 @@ collectFiles d = do
 
 groupObjects :: [WireChunk] -> Map UUID [WireChunk]
 groupObjects chunks =
-    Map.fromListWith (++) [(chunkObject chunk, [chunk])| chunk <- chunks]
+    Map.fromListWith (++) [(chunkObject chunk, [chunk]) | chunk <- chunks]
 
 reduceAndCompareChunks :: [WireChunk] -> [WireChunk] -> Property
 reduceAndCompareChunks chunksIn chunksOut =
@@ -124,10 +144,12 @@ sortChunkOps chunk = case chunk of
     _ -> chunk
 
 chunkObject :: WireChunk -> UUID
-chunkObject = objectId . \case
-    Closed op                         -> op
-    Value WireReducedChunk{wrcHeader} -> wrcHeader
-    Query WireReducedChunk{wrcHeader} -> wrcHeader
+chunkObject =
+    objectId
+        . \case
+            Closed op -> op
+            Value WireReducedChunk{wrcHeader} -> wrcHeader
+            Query WireReducedChunk{wrcHeader} -> wrcHeader
 
 isRelevant :: WireChunk -> Bool
 isRelevant = \case
@@ -138,7 +160,7 @@ isRelevant = \case
 
 collectLefts :: (Either a b, Either a b) -> Either [(String, a)] (b, b)
 collectLefts = \case
-    (Left a1,  Left a2 ) -> Left [("in",  a1), ("out", a2)]
-    (Left a,   Right _ ) -> Left [("in",  a)]
-    (Right _,  Left a  ) -> Left [("out", a)]
+    (Left a1, Left a2) -> Left [("in", a1), ("out", a2)]
+    (Left a, Right _) -> Left [("in", a)]
+    (Right _, Left a) -> Left [("out", a)]
     (Right b1, Right b2) -> Right (b1, b2)
--- ron-test/HexDump.hs
+++ ron-test/HexDump.hs
@@ -1,29 +1,37 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module HexDump where
 
-import           RON.Prelude
+import RON.Prelude
 
-import qualified Data.ByteString.Lazy as BSL
-import           Data.List (init, tail)
-import           Numeric (showHex)
+import Data.ByteString.Lazy qualified as BSL
+import Data.List (init)
+import Numeric (showHex)
 
 hexdump :: BSL.ByteString -> String
 hexdump = unlines . map showLine . split
   where
     showLine s =
-        show8 s1 ++ "  " ++ show8 s2 ++ " | " ++
-        init (tail $ show $ BSL.map (sub 0 32) s)
+        show8 s1
+            ++ "  "
+            ++ show8 s2
+            ++ " | "
+            ++ init (drop 1 $ show $ BSL.map (sub 0 32) s)
       where
         (s1, s2) = BSL.splitAt 8 s
+
     show8 s = unwords (map showHex2 bs) ++ replicate (24 - 3 * length bs) ' '
       where
         bs = BSL.unpack s
+
     showHex2 b
-        | b < 16    = '0' : showHex b ""
+        | b < 16 = '0' : showHex b ""
         | otherwise = showHex b ""
+
     split s = case BSL.splitAt 16 s of
-        ("",    _   ) -> []
-        (chunk, ""  ) -> [chunk]
+        ("", _) -> []
+        (chunk, "") -> [chunk]
         (chunk, rest) -> chunk : split rest
+
     sub x y z = if x == z then y else z
