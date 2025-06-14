6efce415878ace8b5ffddab9026076bf319e087f
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-06-14 14:17:24 +0200

    Style

--- ron-test/LwwStruct.hs
+++ ron-test/LwwStruct.hs
@@ -49,16 +49,13 @@ replica = mkReplica ApplicationSpecific 0xd83d30067100000
 
 ex1expect :: ByteStringL
 ex1expect =
-    [s|
-        *lww    #7/0000000DrW+r3pl1c4                   !
+    [s| *lww    #7/0000000DrW+r3pl1c4                   !
                                         @`      :int1   275
                                                 :nst5
                                                 :set4   >}KUW
                                                 :str2   >}OUW
                                                 :str3   '190'
-
         *set    #}KUW                   @0      :0      !
-
         *rga    #}OUW                                   !
                                         @`}WUW          '2'
                                         @)X             '7'
@@ -68,33 +65,27 @@ ex1expect =
 
 ex4expect :: ByteStringL
 ex4expect =
-    [s|
-        *lww    #7/0000000DrW+r3pl1c4                   !
+    [s| *lww    #7/0000000DrW+r3pl1c4                   !
                                         @`}WUW  :int1   166
                                         @{1p0W  :nst5
                                         @`      :set4   >}KUW
                                                 :str2   >}OUW
                                         @{19UW  :str3   '206'
-
         *set    #}KUW                   @0      :0      !
                                         @`{1kmp         >{1AUW
-
         *rga    #}OUW                   @0              !
                                         @`}WUW  :`}kmp  '2'
                                         @)X     :}p0W   '7'
                                         @{112W  :0      '1'
                                         @}6MW           '4'
                                         @{0WUY          '5'
-
         *lww    #{1AUW                  @0              !
                                         @`      :int1   135
                                                 :nst5
                                                 :set4   >}KUW
                                                 :str2   >}OUW
                                                 :str3   '137'
-
         *set    #}KUW                   @0      :0      !
-
         *rga    #}OUW                                   !
                                         @`}WUW          '1'
                                         @)X             '3'
@@ -140,28 +131,30 @@ prop_lwwStruct = property do
 
     -- apply operations to the object (frame)
     ex4state <-
-        evalExceptT $
-            runNetworkSimT $
-                runReplicaSimT replica $
-                    execObjectState ex2state do
-                        -- plain field
-                        int1_set $ Just 166
-                        str2_zoom $ RGA.edit "145"
-                        str3Value <- str3_read
-                        str3Value === Just "190"
-                        str3_set $ Just "206"
-                        set4_zoom $
-                            ORSet.addValue
-                                Struct51
-                                    { int1 = Just 135
-                                    , str2 = Just $ RGA "136"
-                                    , str3 = Just "137"
-                                    , set4 = Just $ ORSet []
-                                    , nst5 = Nothing
-                                    }
-                        nst5Value <- nst5_read
-                        nst5Value === Nothing
-                        nst5_set Nothing
+        ( evalExceptT
+                . runNetworkSimT
+                . runReplicaSimT replica
+                . execObjectState ex2state
+            )
+            do
+                -- plain field
+                int1_set $ Just 166
+                str2_zoom $ RGA.edit "145"
+                str3Value <- str3_read
+                str3Value === Just "190"
+                str3_set $ Just "206"
+                set4_zoom $
+                    ORSet.addValue
+                        Struct51
+                            { int1 = Just 135
+                            , str2 = Just $ RGA "136"
+                            , str3 = Just "137"
+                            , set4 = Just $ ORSet []
+                            , nst5 = Nothing
+                            }
+                nst5Value <- nst5_read
+                nst5Value === Nothing
+                nst5_set Nothing
 
     -- decode object after modification
     example4 <- evalEither $ evalObjectState ex4state readObject
@@ -170,5 +163,6 @@ prop_lwwStruct = property do
     -- serialize object after modification
     prep ex4expect === prep (snd $ serializeObject ex4state)
     parseObject oid ex4expect === Right ex4state
-  where
-    prep = filter (not . null) . map BSLC.words . BSLC.lines
+
+prep :: ByteStringL -> [ByteStringL]
+prep = filter (not . BSLC.null) . map (BSLC.unwords . BSLC.words) . BSLC.lines
