271c2148cf2d1c9cda79f08aa59c765db6fd5ec2
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-06-15 16:57:09 +0200

    Style

--- ron/lib/RON/Text/Serialize.hs
+++ ron/lib/RON/Text/Serialize.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE BinaryLiterals #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -19,31 +21,44 @@ module RON.Text.Serialize (
     serializeWireFrames,
     uuidToString,
     uuidToText,
-    ) where
+) where
 
-import           RON.Prelude hiding (elem)
+import RON.Prelude hiding (elem)
 
-import           Control.Monad.State.Strict (state)
-import qualified Data.Aeson as Json
-import           Data.ByteString.Lazy.Char8 (cons, elem, snoc)
-import qualified Data.ByteString.Lazy.Char8 as BSL
-import qualified Data.Map.Strict as Map
+import Control.Monad.State.Strict (state)
+import Data.Aeson qualified as Json
+import Data.ByteString.Lazy.Char8 (cons, elem, snoc)
+import Data.ByteString.Lazy.Char8 qualified as BSL
+import Data.Map.Strict qualified as Map
 
-import           RON.Text.Serialize.UUID (serializeUuid, serializeUuidAtom,
-                                          serializeUuidKey, uuidToString,
-                                          uuidToText)
-import           RON.Types (Atom (AFloat, AInteger, AString, AUuid),
-                            ClosedOp (..), ObjectFrame (..), Op (..), Payload,
-                            StateFrame, WireChunk (Closed, Query, Value),
-                            WireFrame, WireReducedChunk (..),
-                            WireStateChunk (..))
-import           RON.UUID (UUID, zero)
-import qualified RON.UUID as UUID
+import RON.Text.Serialize.UUID (
+    serializeUuid,
+    serializeUuidAtom,
+    serializeUuidKey,
+    uuidToString,
+    uuidToText,
+ )
+import RON.Types (
+    Atom (AFloat, AInteger, AString, AUuid),
+    ClosedOp (ClosedOp),
+    ObjectFrame (ObjectFrame),
+    Op (Op),
+    Payload,
+    StateFrame,
+    WireChunk (Closed, Query, Value),
+    WireFrame,
+    WireReducedChunk (WireReducedChunk),
+    WireStateChunk (WireStateChunk),
+    objectId,
+    reducerId,
+ )
+import RON.Types qualified
+import RON.UUID (UUID, succValue, zero)
 
 -- | Serialize a common frame
 serializeWireFrame :: WireFrame -> ByteStringL
 serializeWireFrame =
-  (`snoc` '.') . fold . (`evalState` opZero) . traverse serializeChunk
+    (`snoc` '.') . fold . (`evalState` closedOpZero) . traverse serializeChunk
 
 -- | Serialize a sequence of common frames
 serializeWireFrames :: [WireFrame] -> ByteStringL
@@ -52,91 +67,91 @@ serializeWireFrames = foldMap serializeWireFrame
 -- | Serialize a common chunk
 serializeChunk :: WireChunk -> State ClosedOp ByteStringL
 serializeChunk = \case
-  Closed op -> (<> " ;\n") <$> serializeClosedOpZip op
-  Value chunk -> serializeReducedChunk False chunk
-  Query chunk -> serializeReducedChunk True chunk
+    Closed op -> (<> " ;\n") <$> serializeClosedOpZip op
+    Value chunk -> serializeReducedChunk False chunk
+    Query chunk -> serializeReducedChunk True chunk
 
 -- | Serialize a reduced chunk
 serializeReducedChunk :: Bool -> WireReducedChunk -> State ClosedOp ByteStringL
-serializeReducedChunk isQuery WireReducedChunk {wrcHeader, wrcBody} =
-  BSL.unlines <$> liftA2 (:) serializeHeader serializeBody
+serializeReducedChunk isQuery WireReducedChunk{wrcHeader, wrcBody} =
+    BSL.unlines <$> liftA2 (:) serializeHeader serializeBody
   where
     serializeHeader = do
-      h <- serializeClosedOpZip wrcHeader
-      pure $ BSL.intercalate "\t" [h, if isQuery then "?" else "!"]
-    serializeBody = state $ \ClosedOp {op = opBefore, ..} ->
-      let (body, opAfter) =
-            (`runState` opBefore)
-              $ for wrcBody
-              $ fmap ("\t" <>)
-              . serializeReducedOpZip objectId
-       in (body, ClosedOp {op = opAfter, ..})
+        h <- serializeClosedOpZip wrcHeader
+        pure $ BSL.intercalate "\t" [h, if isQuery then "?" else "!"]
+    serializeBody = state \ClosedOp{op = opBefore, ..} ->
+        let (body, opAfter) =
+                (`runState` opBefore) $
+                    for wrcBody $
+                        fmap ("\t" <>)
+                            . serializeReducedOpZip objectId
+         in (body, ClosedOp{op = opAfter, ..})
 
 -- | Serialize a context-free raw op
 serializeRawOp :: ClosedOp -> ByteStringL
-serializeRawOp op = evalState (serializeClosedOpZip op) opZero
+serializeRawOp op = evalState (serializeClosedOpZip op) closedOpZero
 
 -- | Serialize a raw op with compression in stream context
 serializeClosedOpZip :: ClosedOp -> State ClosedOp ByteStringL
-serializeClosedOpZip this = state $ \prev ->
-  let prev' = op prev
-      typ = serializeUuidKey (reducerId prev) zero (reducerId this)
-      obj = serializeUuidKey (objectId prev) (reducerId this) (objectId this)
-      evt = serializeUuidKey (opId prev') (objectId this) (opId this')
-      ref = serializeUuidKey (refId prev') (opId this') (refId this')
-      payloadAtoms = serializePayloadZip (objectId this) (payload this')
-   in ( BSL.intercalate "\t"
-          $  key '*' typ
-          ++ key '#' obj
-          ++ key '@' evt
-          ++ key ':' ref
-          ++ [payloadAtoms | not $ BSL.null payloadAtoms],
-        this
+serializeClosedOpZip this = state \prev ->
+    let typ = serializeUuidKey prev.reducerId zero this.reducerId
+        obj = serializeUuidKey prev.objectId this.reducerId this.objectId
+        evt = serializeUuidKey prev.op.opId this.objectId this.op.opId
+        ref = serializeUuidKey prev.op.refId this.op.opId this.op.refId
+        payloadAtoms = serializePayloadZip this.objectId this.op.payload
+     in ( BSL.intercalate "\t" $
+            key '*' typ
+                ++ key '#' obj
+                ++ key '@' evt
+                ++ key ':' ref
+                ++ [payloadAtoms | not $ BSL.null payloadAtoms]
+        , this
         )
   where
-    this' = op this
     key c u = [c `cons` u | not $ BSL.null u]
 
 -- | Serialize a reduced op with compression in stream context
-serializeReducedOpZip
-  :: UUID -- ^ enclosing object
-  -> Op
-  -> State Op ByteStringL
-serializeReducedOpZip opObject this = state $ \prev ->
-  let evt = serializeUuidKey (opId prev) opObject (opId this)
-      ref = serializeUuidKey (refId prev) (opId this) (refId this)
-      payloadAtoms = serializePayloadZip opObject (payload this)
-      keys
-        | BSL.null evt && BSL.null ref = ["@"]
-        | otherwise = key '@' evt ++ key ':' ref
-      op = keys ++ [payloadAtoms | not $ BSL.null payloadAtoms]
-   in (BSL.intercalate "\t" op, this)
+serializeReducedOpZip ::
+    -- | enclosing object
+    UUID ->
+    Op ->
+    State Op ByteStringL
+serializeReducedOpZip opObject this = state \prev ->
+    let evt = serializeUuidKey prev.opId opObject this.opId
+        ref = serializeUuidKey prev.refId this.opId this.refId
+        payloadAtoms = serializePayloadZip opObject this.payload
+        keys
+            | BSL.null evt && BSL.null ref = ["@"]
+            | otherwise = key '@' evt ++ key ':' ref
+        op = keys ++ [payloadAtoms | not $ BSL.null payloadAtoms]
+     in (BSL.intercalate "\t" op, this)
   where
     key c u = [c `cons` u | not $ BSL.null u]
 
 serializeOp :: Op -> ByteStringL
 serializeOp Op{opId, refId, payload} =
-  BSL.intercalate "\t"
-    [ '@' `cons` serializeUuid opId
-    , ':' `cons` serializeUuid refId
-    , serializePayloadZip opId payload
-    ]
+    BSL.intercalate
+        "\t"
+        [ '@' `cons` serializeUuid opId
+        , ':' `cons` serializeUuid refId
+        , serializePayloadZip opId payload
+        ]
 
 serializeOpenOp ::
-  -- | Previous op id
-  UUID ->
-  -- | Current op
-  Op ->
-  ByteStringL
+    -- | Previous op id
+    UUID ->
+    -- | Current op
+    Op ->
+    ByteStringL
 serializeOpenOp prevId Op{opId, refId, payload} =
-  BSL.intercalate "\t" $ idS : refS : payloadS
+    BSL.intercalate "\t" $ idS : refS : payloadS
   where
     idS
-      | opId /= UUID.succValue prevId = '@' `cons` serializeUuid opId
-      | otherwise                     = ""
+        | opId /= succValue prevId = '@' `cons` serializeUuid opId
+        | otherwise = ""
     refS
-      | refId /= prevId = ':' `cons` serializeUuid refId
-      | otherwise       = ""
+        | refId /= prevId = ':' `cons` serializeUuid refId
+        | otherwise = ""
     payloadS = [serializePayloadZip opId payload | not $ null payload]
 
 -- | Serialize a context-free atom
@@ -146,74 +161,79 @@ serializeAtom a = evalState (serializeAtomZip a) zero
 -- | Serialize an atom with compression for UUID in stream context
 serializeAtomZip :: Atom -> State UUID ByteStringL
 serializeAtomZip = \case
-  AFloat f -> pure $ serializeFloatAtom f
-  AInteger i -> pure $ serializeIntegerAtom i
-  AString s -> pure $ serializeString s
-  AUuid u -> serializeUuidAtom' u
+    AFloat f -> pure $ serializeFloatAtom f
+    AInteger i -> pure $ serializeIntegerAtom i
+    AString s -> pure $ serializeString s
+    AUuid u -> serializeUuidAtom' u
 
--- | Serialize a float atom.
--- If unambiguous, i.e. contains a '.' or an 'e'/'E', the prefix '^' is skipped.
+{- | Serialize a float atom.
+If unambiguous, i.e. contains a '.' or an 'e'/'E', the prefix '^' is skipped.
+-}
 serializeFloatAtom :: Double -> ByteStringL
 serializeFloatAtom float
-  | isDistinguishableFromUuid = bs
-  | otherwise = '^' `cons` bs
+    | isDistinguishableFromUuid = bs
+    | otherwise = '^' `cons` bs
   where
     isDistinguishableFromUuid = '.' `elem` bs || 'e' `elem` bs || 'E' `elem` bs
     bs = BSL.pack $ show float
 
--- | Serialize an integer atom.
--- Since integers are always unambiguous, the prefix '=' is always skipped.
+{- | Serialize an integer atom.
+Since integers are always unambiguous, the prefix '=' is always skipped.
+-}
 serializeIntegerAtom :: Int64 -> ByteStringL
 serializeIntegerAtom = BSL.pack . show
 
 -- | Serialize a string atom
 serializeString :: Text -> ByteStringL
 serializeString =
-  wrapSingleQuotes . escapeApostrophe . stripDoubleQuotes . Json.encode
+    wrapSingleQuotes . escapeApostrophe . stripDoubleQuotes . Json.encode
   where
     wrapSingleQuotes = (`snoc` '\'') . cons '\''
     stripDoubleQuotes = BSL.init . BSL.tail
     escapeApostrophe s
-      | BSL.null s2 = s1
-      | otherwise = s1 <> "\\'" <> escapeApostrophe (BSL.tail s2)
+        | BSL.null s2 = s1
+        | otherwise = s1 <> "\\'" <> escapeApostrophe (BSL.tail s2)
       where
         (s1, s2) = BSL.break (== '\'') s
 
 serializeUuidAtom' :: UUID -> State UUID ByteStringL
 serializeUuidAtom' u =
-  -- TODO(2019-08-19, cblp): Check if uuid can be unambiguously serialized and
-  -- if so, skip the prefix.
-  state $ \prev -> (cons '>' $ serializeUuidAtom prev u, u)
+    -- TODO(2019-08-19, cblp): Check if uuid can be unambiguously serialized and
+    -- if so, skip the prefix.
+    state \prev -> (cons '>' $ serializeUuidAtom prev u, u)
 
 -- | Serialize a payload in stream context
-serializePayloadZip
-  :: UUID -- ^ previous UUID (default is 'zero')
-  -> Payload
-  -> ByteStringL
+serializePayloadZip ::
+    -- | previous UUID (default is 'zero')
+    UUID ->
+    Payload ->
+    ByteStringL
 serializePayloadZip prev =
-  BSL.unwords . (`evalState` prev) . traverse serializeAtomZip
+    BSL.unwords . (`evalState` prev) . traverse serializeAtomZip
 
 -- | Serialize an abstract payload
 serializePayload :: Payload -> ByteStringL
-serializePayload = serializePayloadZip UUID.zero
+serializePayload = serializePayloadZip zero
 
 -- | Serialize a state frame
 serializeStateFrame :: StateFrame -> ByteStringL
 serializeStateFrame = serializeWireFrame . map wrapChunk . Map.assocs
   where
-    wrapChunk (objectId, WireStateChunk {stateType, stateBody}) =
-      Value WireReducedChunk
-        { wrcHeader = opZero {reducerId = stateType, objectId},
-          wrcBody = stateBody
-          }
+    wrapChunk (objectId, WireStateChunk{stateType, stateBody}) =
+        Value
+            WireReducedChunk
+                { wrcHeader = closedOpZero{reducerId = stateType, objectId}
+                , wrcBody = stateBody
+                }
 
 -- | Serialize an object. Return object id that must be stored separately.
 serializeObject :: ObjectFrame a -> (UUID, ByteStringL)
 serializeObject (ObjectFrame oid frame) = (oid, serializeStateFrame frame)
 
-opZero :: ClosedOp
-opZero = ClosedOp
-  { reducerId = zero
-  , objectId  = zero
-  , op        = Op{opId = zero, refId = zero, payload = []}
-  }
+closedOpZero :: ClosedOp
+closedOpZero =
+    ClosedOp
+        { reducerId = zero
+        , objectId = zero
+        , op = Op{opId = zero, refId = zero, payload = []}
+        }
