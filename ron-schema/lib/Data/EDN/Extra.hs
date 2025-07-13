e7dbf14b46d1a203221ef8266a91264322c80694
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-07-13 11:23:39 +0200

    Style

--- ron-schema/lib/Data/EDN/Extra.hs
+++ ron-schema/lib/Data/EDN/Extra.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -14,23 +13,30 @@ module Data.EDN.Extra (
     withSymbol',
 ) where
 
-import           RON.Prelude
+import RON.Prelude
 
-import qualified Data.EDN.AST.Lexer as EdnAst
-import qualified Data.EDN.AST.Parser as EdnAst
-import qualified Data.EDN.AST.Types as EdnAst
-import           Data.EDN.Class.Parser (Parser)
-import           Text.Megaparsec (SourcePos (..), mkPos)
-import qualified Text.Megaparsec as Mpc
+import Data.EDN.AST.Lexer qualified as EdnAst
+import Data.EDN.AST.Parser qualified as EdnAst
+import Data.EDN.AST.Types qualified as EdnAst
+import Data.EDN.Class.Parser (Parser)
+import Text.Megaparsec (SourcePos (..), mkPos)
+import Text.Megaparsec qualified as Mpc
 
-import           Data.EDN (EDNList, FromEDN, Tagged (NoTag, Tagged),
-                           TaggedValue, Value (List), parseEDNv, withNoTag,
-                           withSymbol)
+import Data.EDN (
+    EDNList,
+    FromEDN,
+    Tagged (NoTag, Tagged),
+    TaggedValue,
+    Value (List),
+    parseEDNv,
+    withNoTag,
+    withSymbol,
+ )
 
-withNoPrefix :: MonadFail m => (Text -> m a) -> Text -> Text -> m a
+withNoPrefix :: (MonadFail m) => (Text -> m a) -> Text -> Text -> m a
 withNoPrefix f prefix name = case prefix of
     "" -> f name
-    _  -> fail "Expected no prefix"
+    _ -> fail "Expected no prefix"
 
 withSymbol' :: (Text -> Parser a) -> TaggedValue -> Parser a
 withSymbol' = withNoTag . withSymbol . withNoPrefix
@@ -40,32 +46,31 @@ parseSymbol' = withSymbol' pure
 
 isTagged :: Tagged t a -> Bool
 isTagged = \case
-    NoTag {} -> False
+    NoTag{} -> False
     Tagged{} -> True
 
 parseMultiDoc :: EdnAst.Parser [TaggedValue]
 parseMultiDoc = EdnAst.dropWS *> many EdnAst.parseTagged <* Mpc.eof
 
-decodeMultiDoc :: MonadFail m => SourcePos -> Text -> m [TaggedValue]
+decodeMultiDoc :: (MonadFail m) => SourcePos -> Text -> m [TaggedValue]
 decodeMultiDoc sourcePos input =
-    either (fail . Mpc.errorBundlePretty) pure
-    $ snd
-    $ Mpc.runParser' parseMultiDoc initState
+    either (fail . Mpc.errorBundlePretty) pure . snd $
+        Mpc.runParser' parseMultiDoc initState
   where
-    initState = Mpc.State{
-        stateInput       = input,
-        stateOffset      = 0,
-#if MIN_VERSION_megaparsec(8,0,0)
-        stateParseErrors = [],
-#endif
-        statePosState = Mpc.PosState{
-            pstateInput      = input,
-            pstateOffset     = 0,
-            pstateSourcePos  = sourcePos,
-            pstateTabWidth   = Mpc.defaultTabWidth,
-            pstateLinePrefix = ""
-        }
-    }
+    initState =
+        Mpc.State
+            { stateInput = input
+            , stateOffset = 0
+            , stateParseErrors = []
+            , statePosState =
+                Mpc.PosState
+                    { pstateInput = input
+                    , pstateOffset = 0
+                    , pstateSourcePos = sourcePos
+                    , pstateTabWidth = Mpc.defaultTabWidth
+                    , pstateLinePrefix = ""
+                    }
+            }
 
-parseList :: FromEDN a => EDNList -> Parser a
+parseList :: (FromEDN a) => EDNList -> Parser a
 parseList = parseEDNv . List
